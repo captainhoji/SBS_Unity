7ff763e67c64ac64beee87bf48f2a45459e05600
Jiho Kim <jihok@usc.edu>
2020-05-09 11:59:08 +0900

    ruby init

---
+++ UNITY_Week1/Assets/Scripts/UserName.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserName : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    int number = 0;
+    char ch = 'A';
+    string str = "Hello World";
+    bool isTrue = true;
+    float f = 0.4912838f;
+
+    void Start()
+    {
+        Debug.Log("number: " + number);
+        Debug.Log("ch: " + ch);
+        Debug.Log("str: " + str);
+        Debug.Log("isTrue: " + isTrue);
+        Debug.Log("f: " + f);
+
+        for (int i = 1; i <= 9; ++i)
+        {
+            Debug.Log(2 + " x " + i + " = " + (2 * i));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
